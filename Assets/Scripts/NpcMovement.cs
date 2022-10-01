bd69d51fc5d3f9a56a22d29fc088c5e4b2f261f7
kiwigugu <zhikaizhan17@gmail.com>
2022-10-01 15:48:06 -0400

    characters

---
+++ Assets/Scripts/NpcMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcMovement : MonoBehaviour
+{
+    //Transform that NPC has to follow
+    public Transform transformToFollow;
+    //NavMesh Agent variable
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Follow the player
+        agent.destination = transformToFollow.position;
+    }
+}
