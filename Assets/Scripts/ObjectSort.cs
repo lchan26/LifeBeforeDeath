33a084d2587adb514274e370f28cc247b2c5d579
jakobwismar <jakob_wismar@brown.edu>
2022-10-15 14:41:10 -0400

    sprite sorting order

--- Assets/Scripts/ObjectSort.cs
+++ Assets/Scripts/ObjectSort.cs
@@ -5,28 +5,39 @@ using UnityEngine;
 public class ObjectSort : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private bool topLeft;
+    [SerializeField] private bool isTopLeft;
     private SpriteRenderer sprite;
 
-    private Vector3 colliderBounds;
+    private Collider col;
     private Camera cam;
     void Start()
     {
         sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
         cam = Camera.main;
-        colliderBounds = this.GetComponent<BoxCollider>().bounds.size;
+        col = this.GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        sprite.sortingOrder = (int)(cam.WorldToScreenPoint(transform.position).y * -100);
+        Vector3 topLeft = new Vector3(col.bounds.center.x - col.bounds.size.x / 2, 0, col.bounds.center.z + col.bounds.size.z / 2);
+        Vector3 bottomRight = new Vector3(col.bounds.center.x + col.bounds.size.x / 2, 0, col.bounds.center.z + col.bounds.size.z / 2);
+
+        if (isTopLeft)
+        {
+            sprite.sortingOrder = (int)(cam.WorldToScreenPoint(topLeft).y * -100);
+        }
+        else
+        {
+            sprite.sortingOrder = (int)(cam.WorldToScreenPoint(bottomRight).y * -100);
+        }
     }
 
 
     void OnDrawGizmos()
     {
-        
+        Vector3 topLeft = new Vector3(col.bounds.center.x - col.bounds.size.x / 2, 0, col.bounds.center.z + col.bounds.size.z / 2);
+        Gizmos.DrawSphere(topLeft, .1f);
     }
 
 }
