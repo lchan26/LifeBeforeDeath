974182f7585f1869739bfc9a50abc5e49ddb4f2e
jakobwismar <jakob_wismar@brown.edu>
2022-10-22 14:53:47 -0400

    Added basic chore

---
+++ Assets/Scripts/Chore.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chore : MonoBehaviour
+{
+    [HideInInspector] public bool completed = false;
+
+    private bool inPlayerRange = false;
+
+    private ChoreManager choreManager;
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        
+        choreManager = FindObjectOfType<ChoreManager>().GetComponent<ChoreManager>();
+        if (choreManager.Equals(null))
+        {
+            Debug.Log("ChoreManager not found in scene!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!completed && inPlayerRange && Input.GetKeyDown(KeyCode.Space))
+        {
+            completed = true;
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+            choreManager.onChoreComplete();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inPlayerRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inPlayerRange = false;
+        }
+    }
+}
