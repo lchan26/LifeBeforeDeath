7dace35d24af16d086a0d3a7f762d89892cad96e
Lucas Chan <lucas_chan@brown.edu>
2022-10-29 15:46:00 -0400

    She teleports

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed = 5;
+    public bool inBotStair = false;
+    public bool inTopStair = false;
 
     // Update is called once per frame
     void Update()
@@ -34,5 +36,78 @@ public class PlayerController : MonoBehaviour
         }
 
         gameObject.GetComponent<Rigidbody>().velocity = speed * dir.normalized;
+
+        if (inBotStair == true) 
+        {
+            // Vector3 currPos = transform.position;
+            // Vector3 moveTo = new Vector3(-19.1170f, 0.0f, 4.3f);
+            // // Vector3 translation = t
+            // // transform.Translate()
+            float step = speed * Time.deltaTime;
+            // Vector3 moveTo1 = new Vector3(-19.1170f, 0.0f, 4.3f);
+            // if (transform.position != moveTo1)
+            // {
+            //     transform.position = Vector3.MoveTowards(transform.position, moveTo1, step);   
+            // }
+            // transform.position = Vector3.MoveTowards(transform.position, moveTo, step);
+            Vector3 moveTo = new Vector3(-22.590f, 0.0f, 14.172f);
+            transform.position = Vector3.MoveTowards(transform.position, moveTo, step);
+        }
+        if (inTopStair == true) 
+        {
+            // Vector3 currPos = transform.position;
+            // Vector3 moveTo = new Vector3(-19.1170f, 0.0f, 4.3f);
+            // // Vector3 translation = t
+            // // transform.Translate()
+            float step = speed * Time.deltaTime;
+            // Vector3 moveTo1 = new Vector3(-22.590f, 0.0f, 14.172f);
+            // if (transform.position != moveTo1)
+            // {
+            //     transform.position = Vector3.MoveTowards(transform.position, moveTo1, step);   
+            // }
+            // transform.position = Vector3.MoveTowards(transform.position, moveTo, step);
+            Vector3 moveTo = new Vector3(-19.1170f, 0.0f, 4.3f);
+            transform.position = Vector3.MoveTowards(transform.position, moveTo, step);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("StairBottom"))
+        {
+            if (inTopStair == true)
+            {
+                inTopStair = false;
+            }
+            else
+            {
+                inBotStair = true;
+            }
+        }
+
+        else if (other.CompareTag("StairTop"))
+        {
+            if (inBotStair == true)
+            {
+                inBotStair = false;
+            }
+            else
+            {
+                inTopStair = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // if (other.CompareTag("StairBottom"))
+        // {
+        //     inBotStair = false;
+        // }
+
+        // if (other.CompareTag("StairTop"))
+        // {
+        //     inTopStair = false;
+        // }
     }
 }
